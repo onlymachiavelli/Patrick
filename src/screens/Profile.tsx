a2e0f3cedd11e02fffda0a7dc26cb16298065c8b
Alaa Barka <tmakaveli643@gmail.com>
2023-07-16 00:04:18 +0100

    profile UI

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -5,6 +5,8 @@ import { profile } from '../styles'
 import Lottie from 'lottie-react-native'
 import Male from './../lottieFiles/Male.json'
 import {InfoCard} from './../components'
+import { LinearGradient } from 'expo-linear-gradient'
+import {AntDesign, Entypo,FontAwesome   } from '@expo/vector-icons'
 const Profile = () =>{
 
 
@@ -81,7 +83,93 @@ const Profile = () =>{
     const animationRef = React.useRef(null)
     return (
         <Native.View style={profile.container}>
-            
+                
+            <LinearGradient
+            style={profile.cov}
+        // Background Linear Gradient
+        colors={['#87CEEB', '#1E90FF']}
+
+        //degree 45degree 
+        start={{ x: 0, y: 0 }}
+      >
+      
+        <Native.View style={profile.proInfo}>
+            <Native.View style={profile.profileBorder}>
+                <Lottie
+                    
+                    source={Male}
+                    loop
+                    autoPlay
+                    style={{
+                        width:110,
+                        height:110
+                        ,}}
+                    ref={animationRef}
+
+                    />
+            </Native.View>
+
+            <Native.View>
+                <Native.Text style={profile.name}>
+                    Alaa Eddine Barka
+                </Native.Text>
+
+                <Native.Text style={profile.id}>
+                <AntDesign name="user" size={10} color="white" /> ID:@123456789
+                </Native.Text>
+                <Native.Text style={profile.data}>
+                    <AntDesign name="enviroment" size={13} color="white" /> 24 Rue Wadhraf Darfadhal Soukra
+                </Native.Text>
+
+                <Native.Text style={profile.data}>
+                    <FontAwesome name="phone" size={15} color="white" /> 54324884
+                </Native.Text>
+
+                <Native.Text style={profile.data}>
+                    
+                    <Native.View style={{
+                        width : 10, 
+                        height : 10,
+                        borderRadius:100, 
+                        backgroundColor:"#B5170D",
+                        
+                    }}>
+                        
+                    </Native.View>
+
+                     {"  "} Device is Disconnected
+                </Native.Text>
+            </Native.View>
+
+
+            <Native.View></Native.View>
+            <Native.View></Native.View>
+            <Native.View></Native.View>
+            <Native.View style={{
+                alignContent:"flex-end",
+                justifyContent:"flex-end",
+                zIndex:100
+
+            }}>
+                <Native.TouchableOpacity
+                    style={{
+                        position:"absolute",
+                        right:0,
+                        top:-50
+                    }}
+                >
+                    
+                    <Entypo  name="menu" size={30} color="white" />    
+                </Native.TouchableOpacity>
+            </Native.View>
+        </Native.View>
+
+        <Native.TouchableOpacity style={profile.editButton}>
+            <Native.Text style={profile.editText}>
+                Edit Profile
+            </Native.Text>
+        </Native.TouchableOpacity>
+      </LinearGradient>
         </Native.View>
     )
 }
--- src/styles/profile.ts
+++ src/styles/profile.ts
@@ -12,61 +12,74 @@ import {
     container:{
         width : "100%",
         height , 
-        backgroundColor:"white"
-
+        backgroundColor:"white",
     },
     cov :{
         width : "100%" , 
-        height : 150, 
-        backgroundColor : "#4C9FD5"
+        height : 350, 
+        //shadow 
+        shadowColor: "#000",
+        shadowOffset: {
+        width: 0,
+        height: 0.5,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 6,
+        borderRadius:40,
+        paddingTop:60,
+        padding:10
     }   ,
-    profileContainer:{
-        width:170,
-        height:170, 
-        borderWidth:2,
-        borderColor:"white",
-        display : "flex",
-        alignItems:"center",
-        justifyContent:"center",
-        borderRadius:100,
-        backgroundColor:"white",
-        marginTop:-80,
-        alignSelf:"center"
-    },
-    name: {
-        color:"#555",
-        fontWeight:"bold",
-        fontSize:20,
-        marginTop:10,
-        alignSelf:"center"
+    proInfo:{
+        width : "100%", 
+        display :"flex" , 
+        alignItems:"center", 
+        justifyContent:"space-between",
+        flexDirection:"row",
+        gap :10
 
     },
-    dataContainer:{
+    profileBorder:{
+        width:90,
+        height:90,
+        borderRadius:100,
         display:"flex" , 
-        width :"100%",
         alignItems:"center",
         justifyContent:"center",
-        marginTop:20,
-        gap:10
+        backgroundColor:"white"
     },
-    edit:{
-        width:"70%",
-
+    name :{
+        color:"white" , 
+        fontWeight:"bold",
+        fontSize:20
+    },
+    id:{
+        color:"white" , 
+        fontSize:10,
+        padding:2
+    },
+    data : {
+        color:"white" , 
+        fontSize:14,
+        padding:2
+    },
+    editButton:{
+        width :"60%" , 
         padding:10,
-        borderRadius:100,
-        backgroundColor:"white",
-        display:"flex",
-        alignItems:"center",
+        borderWidth:1 ,
+        borderColor:"white" , 
+        borderRadius:100 ,
+        alignSelf:"center" , 
+        display:"flex" , 
+        alignItems:"center" , 
         justifyContent:"center",
-        marginTop:20,
-        textAlign:"center",
-        borderWidth:1,
-        borderColor:"#4C9FD5",
-        alignSelf:"center"
-
+        backgroundColor:"#ffffff33",
+        marginTop:20
         
-
+    },
+    editText:{
+        color :"white"
     }
+    
 
  })
 
