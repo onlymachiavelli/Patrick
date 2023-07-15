c189dc807927fd943d1eb60989fd6c842fda5db6
Alaa Barka <tmakaveli643@gmail.com>
2023-07-15 13:17:31 +0100

    Init the app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TouchableOpacity, ToastAndroid} from 'react-native';
 
 export default function App() {
+
+  const onClicke = () =>{
+    
+
+    ToastAndroid.show("A pikachu appeared nearby !", ToastAndroid.SHORT);
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+
+
+      <TouchableOpacity onPress={()=>{
+        onClicke()
+      }}>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
