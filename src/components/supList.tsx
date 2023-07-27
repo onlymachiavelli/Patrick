1a017627d284fd22fc9df63a55b89827bb8e2ee9
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-07-27 17:32:08 +0100

    add list of patients

--- src/components/supList.tsx
+++ src/components/supList.tsx
@@ -8,51 +8,66 @@ import Female from './../lottieFiles/Female.json'
 import Lottie from 'lottie-react-native'
 import {AddSuper} from './../components'
 import {Input} from './'
+import { useGetMe, useRelations } from '../hooks'
+import { User } from './svg'
 
 const Supervisors = () =>{
+    const {name, setName, 
+        phone, setPhone,
+        relations, setRelations,
+        SaveRelation,
+        AcceptOrRefuse,
+        GetRelations} = useRelations()
 
+    React.useEffect(()=>{
+        //GetRelations()
+    }
+    ,[])
+
+    
     //animation ref 
     const animation = React.useRef(null)
     return (
         <Native.ScrollView style={relationsStyle.superList}>
+
+            <AddSuper/>
             <Native.Text style={relationsStyle.title}>
                 My SuperVisors
             </Native.Text>
-            <AddSuper/>
+            {
+                [{fullname:"Bob",gender:"male"},
+                 {fullname:"Lola",gender:"female"},
+                 {fullname:"Micheal",gender:"male"},
+                 {fullname:"Yassine",gender:"male"},
+                 {fullname:"Jean",gender:"male"}
+                                            ].map((item : any)=> 
+                                            <Native.View style={relationsStyle.card}>
+                                                
+                                                <Native.View style={
+                                                    {
+                                                        display: "flex" , 
+                                                        alignItems:"center" , 
+                                                        justifyContent:"center" , 
+                                                        flexDirection:"row"
+                                                    }
+                                                }>
+                                                <Lottie
 
+                                                    source={item.gender=="male"?Male:Female}
+                                                    style ={relationsStyle.avatar}
+                                                    autoPlay
+                                                    loop
+                                                    ref={animation}
 
-            <Native.View style={relationsStyle.card}>
-                <Native.View style={
-                    {
-                        display: "flex" , 
-                        alignItems:"center" , 
-                        justifyContent:"center" , 
-                        flexDirection:"row"
-                    }
-                }>
-                <Lottie
+                                                    />
 
-                    source={Male}
-                    style ={relationsStyle.avatar}
-                    autoPlay
-                    loop
-                    ref={animation}
+                                                    <Native.Text style={relationsStyle.name}>
+                                                    {item.fullname}
+                                                    </Native.Text>
 
-                    />
-
-                    <Native.Text style={relationsStyle.name}>
-                    Alaa Eddine Barka
-                    </Native.Text>
-
-                </Native.View>
-            </Native.View>
-
-
-
-
-
-            
-            
+                                                </Native.View>
+                                            </Native.View>
+            )}
         </Native.ScrollView>
     )
 }
