483c6234c4bcd55065a0eb4421d551e3b6dfc659
Alaa Barka <tmakaveli643@gmail.com>
2023-07-27 15:12:37 +0100

    relations between patients and supervisors

--- src/screens/Relations.tsx
+++ src/screens/Relations.tsx
@@ -1,47 +1,44 @@
-import * as Native from 'react-native'
-import * as React from 'react'
+import * as Native from 'react-native';
+import * as React from 'react';
 
-import { relationsStyle } from '../styles'
-import { ProfileHeader , SideMenu, Footer, AddSuper, Supervisors, Patients, } from '../components'
-import { useGetMe, useRelations } from '../hooks'
+import { relationsStyle } from '../styles';
+import { ProfileHeader, SideMenu, Footer, AddSuper, Supervisors, Patients } from '../components';
+import { useGetMe, useRelations } from '../hooks';
 
-const Relations = ({navigation} : any) =>{
+const Relations = ({ navigation }: any) => {
+  const { GetMe, user } = useGetMe(navigation);
+  const [open, setOpen] = React.useState(false);
+  const [display, setDisplay] = React.useState<React.ReactNode | null>(null);
 
-    const {GetMe, user} = useGetMe(navigation)
-    const [open , setOpen] = React.useState(false) 
-    React.useEffect(()=>{
-        GetMe()
-    }, [])
+  React.useEffect(() => {
+    GetMe();
+  }, []);
 
-    return(
-        <Native.View style={relationsStyle.container}>
+  React.useEffect(() => {
+    if (user) {
+      setDisplay(user.user.type === 'patient' ? <Supervisors /> : <Patients />);
+    }
+  }, [user]);
 
-            <ProfileHeader
+  return (
+    <Native.View style={relationsStyle.container}>
+      <ProfileHeader
+        Display={setOpen}
+        Status={open}
+        ToProfile={() => {
+          navigation.navigate('Profile');
+        }}
+        Name={user ? user.user.fullname : 'Loading'}
+        Gender={user ? user.user.gender : 'Loading'}
+      />
 
-            Display={setOpen}
-            Status={open}
-            ToProfile={()=>{
-                navigation.navigate("Profile")
-            }}
-            Name={user ? user.user.fullname : "Loading"}
-            Gender={user ? user.user.gender : "Loading"}
+      {display}
 
+      <Footer />
 
-            />
+      <SideMenu Hide={setOpen} Display={open} />
+    </Native.View>
+  );
+};
 
-            <Patients/>
-
-            <Footer/>
-
-            <SideMenu
-            
-               Hide={
-                     setOpen
-               }
-               Display={open}
-            />
-        </Native.View>
-    )
-}
-
-export default Relations
+export default Relations;
